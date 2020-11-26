04eeee9f5597721f2510ac7d858c81712aa94ea9
Saul Figueiredo <saul.figueiredo-ext@ab-inbev.com>
2020-11-26 10:04:10 -0300

    Added Person Module inside Components

---
+++ code/pages/api/README.md
@@ -0,0 +1,3 @@
+# Basic API routes example
+
+Next.js ships with [API routes](https://nextjs.org/docs/api-routes/introduction) which provides an easy solution to build your own `API`. This example shows how to create multiple `API` endpoints with serverless functions, which can execute independently.
--- code/pages/index.js
+++ code/pages/index.js
@@ -1,5 +1,19 @@
-function HomePage() {
-  return <div>Welcome to Next.js!</div>
-}
+import useSWR from 'swr'
+import Person from '../components/Person'
 
-export default HomePage
+const fetcher = (url) => fetch(url).then((res) => res.json())
+
+export default function Index() {
+  const { data, error } = useSWR('/api/people', fetcher)
+
+  if (error) return <div>Failed to load</div>
+  if (!data) return <div>Loading...</div>
+
+  return (
+    <ul>
+      {data.map((p, i) => (
+        <Person key={i} person={p} />
+      ))}
+    </ul>
+  )
+}
